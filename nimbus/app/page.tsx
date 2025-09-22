0439207fa1e30331c26763aede0401dc1f58bf2e
R7nX <baokhanh0514@gmail.com>
2025-09-22 14:24:27 -0600

    Started working on Save/Load file

--- nimbus/app/page.tsx
+++ nimbus/app/page.tsx
@@ -3,29 +3,84 @@ import { useState } from "react";
 import Editor from "@monaco-editor/react";
 import Header from "@/components/header";
 import Link from "next/link";
+
+
 export default function App() {
-  const [code, setCode] = useState(`def Hello(): \n return "Hello World`);
+  const [code, setCode] = useState<string>(`def Hello(): \n return "Hello World`);
+  const [language, setLanguage] = useState<string>("python");
+  const [theme, setTheme] = useState<string>("vs-dark");
+  const [file, setFileName] = useState<string>("untitled.py");
+  const [isDirty, setIsDirty] = useState<boolean>(false); // Track whether the file is unsaved
+  const fileHandleRef = useRef<FileSystemFileHandle | null> (null);
+  const hiddenFileInputRef = useRef<HTMLInputElement | null> (null);
+
+
   return (
-    <div className="h-screen flex flex-col">
-      <header className="p-3 border-b">VS Lite — Editor</header>
-              {/* Login Button */}
-      <div className="absolute top-4 right-4 z-20">
-        <Link href="/login">
 
-            Log In
-
-        </Link>
-        </div>
-      <div className="flex-1">
-        <Editor
-          height="100%"
-          defaultLanguage="python"
-          theme="vs-dark"
-          value={code}
-          onChange={(v) => setCode(v ?? "")}
-          options={{ fontSize: 14, minimap: { enabled: false } }}
-        />
-      </div>
-    </div>
   );
+}
+
+
+// Check if the browser support Save/Load files
+const supportsFileSystemAccess = useMemo(() => {
+  return typeof window != "undefined" && "showOpenFilePicker" in window;
+}, []);
+
+// Map default file extension, Ex: if you are coding a python file -> the save file will automatically save it as something.py
+const extFromLang = (lang: string) => {
+  switch(lang){
+    case "javascript":
+    case "typescript":
+      return ".ts";
+    case "html";
+      return ".html";
+    case "json":
+      return ".json";
+    case "css":
+      return ".css";
+    case "python":
+      return ".py";
+    default:
+      return ".py";
+  }
+}
+
+
+const pickerTypes = useMemo(() => {
+  return [
+    {
+      description: "Code files",
+      accept: {
+        "text/plain": [
+          ".py",
+          ".ts",
+          ".js",
+          ".json",
+          ".txt",
+          ".html",
+          ".css",
+          ".md",
+        ],
+      },
+    },
+  ];
+}, []);
+
+
+async function saveFile(){
+
+}
+
+
+async function openFile(){
+
+}
+
+async function saveFileAs(){
+
+
+}
+
+async function writeHandle(handle: FileSystemFileHandle, contents: string){
+
 }
